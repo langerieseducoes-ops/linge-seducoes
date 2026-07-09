4ef6525bda80b81ffa5749e169547aa9e4fd2136
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-07-09 10:07:33 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,37 @@
+// Dashboard - Linge & Seduções
 
+function atualizarDashboard() {
+
+    const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
+    let totalProdutos = produtos.length;
+    let totalEstoque = 0;
+    let valorEstoque = 0;
+
+    produtos.forEach(produto => {
+        totalEstoque += Number(produto.quantidade);
+        valorEstoque += Number(produto.quantidade) * Number(produto.custo);
+    });
+
+    const elProdutos = document.getElementById("totalProdutos");
+    const elEstoque = document.getElementById("totalEstoque");
+    const elValor = document.getElementById("valorEstoque");
+
+    if (elProdutos) {
+        elProdutos.innerText = totalProdutos;
+    }
+
+    if (elEstoque) {
+        elEstoque.innerText = totalEstoque;
+    }
+
+    if (elValor) {
+        elValor.innerText = valorEstoque.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+    }
+
+}
+
+window.onload = atualizarDashboard;
