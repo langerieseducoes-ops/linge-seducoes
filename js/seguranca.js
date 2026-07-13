edfd02b92971fb7147db61ca85084628d7dc0dbe
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-07-13 17:40:10 -0300

    Update seguranca.js

--- js/seguranca.js
+++ js/seguranca.js
@@ -3,15 +3,18 @@
 // Segurança do Sistema
 // ======================================
 
-// Verifica se o usuário está logado
-if (localStorage.getItem("usuarioLogado") !== "sim") {
+const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+
+if (!usuarioLogado) {
+
     window.location.href = "index.html";
+
 }
 
-// Função para sair do sistema
-function sair() {
+// Função para sair
+function sair(){
 
-    if (confirm("Deseja realmente sair do sistema?")) {
+    if(confirm("Deseja realmente sair do sistema?")){
 
         localStorage.removeItem("usuarioLogado");
 
