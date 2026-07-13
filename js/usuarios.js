0f750f9c3b3ab23e678d32665b41642b522c37d6
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-07-13 12:41:05 -0300

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,1 +1,45 @@
+// ======================================
+// Linge & Seduções ERP
+// Banco de Usuários
+// ======================================
 
+if (!localStorage.getItem("usuarios")) {
+
+    const usuarios = [
+
+        {
+            usuario: "admin",
+            senha: "1234",
+            nome: "Administrador",
+            nivel: "admin"
+        },
+
+        {
+            usuario: "gerente",
+            senha: "1234",
+            nome: "Gerente",
+            nivel: "gerente"
+        },
+
+        {
+            usuario: "vendedor",
+            senha: "1234",
+            nome: "Vendedor",
+            nivel: "vendedor"
+        },
+
+        {
+            usuario: "estoquista",
+            senha: "1234",
+            nome: "Estoquista",
+            nivel: "estoquista"
+        }
+
+    ];
+
+    localStorage.setItem(
+        "usuarios",
+        JSON.stringify(usuarios)
+    );
+
+}
