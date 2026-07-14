abd080dbacc768e2951ba32f3a383d9beaed568f
langerieseducoes-ops <langerieseducoes@gmail.com>
2026-07-14 12:26:31 -0300

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,13 +1,15 @@
 // ======================================
 // Linge & Seduções ERP
-// Banco de Usuários
+// Módulo de Usuários
 // ======================================
 
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+// Banco de usuários
+
+let usuarios =
+JSON.parse(localStorage.getItem("usuarios")) || [];
 
 // ======================================
-// Cria usuários padrão somente
-// na primeira execução
+// Primeiro acesso
 // ======================================
 
 if (usuarios.length === 0) {
@@ -15,39 +17,31 @@ if (usuarios.length === 0) {
     usuarios = [
 
         {
-            nome: "Administrador",
-            usuario: "admin",
-            senha: "1234",
-            perfil: "Admin",
-            ativo: true
-        },
-
-        {
-            nome: "Gerente",
-            usuario: "gerente",
-            senha: "1234",
-            perfil: "Gerente",
-            ativo: true
-        },
-
-        {
-            nome: "Vendedor",
-            usuario: "vendedor",
-            senha: "1234",
-            perfil: "Vendedor",
-            ativo: true
-        },
-
-        {
-            nome: "Estoquista",
-            usuario: "estoquista",
-            senha: "1234",
-            perfil: "Estoquista",
-            ativo: true
+            id:1,
+            nome:"Administrador",
+            usuario:"admin",
+            senha:"1234",
+            perfil:"Admin",
+            ativo:true,
+            cadastro:new Date().toLocaleDateString("pt-BR"),
+            ultimoLogin:""
         }
 
     ];
 
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    salvarUsuarios();
+
+}
+
+// ======================================
+// Salvar Banco
+// ======================================
+
+function salvarUsuarios(){
+
+    localStorage.setItem(
+        "usuarios",
+        JSON.stringify(usuarios)
+    );
 
 }
